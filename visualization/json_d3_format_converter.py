550e9efc4d7086bc3ae4b8bea34407e339b80138
David <dave@dyn1177-118.wlan.ic.ac.uk>
2016-03-31 19:58:10 +0100

    modified json converter, updated fdg for cache size

--- visualization/json_d3_format_converter.py
+++ visualization/json_d3_format_converter.py
@@ -13,28 +13,42 @@ def writefile(filename, output):
     f.close
 
 
-json_string = readfile('../icarus/examples/JSON_results_test/results_topology_0.json')
+#json_string = readfile('../icarus/examples/JSON_results_test/results_topology_0.json')
+json_string = readfile('../fixtures/GeantTopology.json')
+#json_string = readfile('../fixtures/6NodeTopology.json')
 parsed_json = json.loads(json_string)
 
 nodes = []
 i = 0
 for key in parsed_json['nodes']:
-    #print parsed_json['nodes'][key]
-    #print parsed_json['nodes'][key]['py/tuple']
-    #print parsed_json['nodes'][key]['py/tuple'][0]
-    nodes.append({"name": key, "group":i, "type": parsed_json['nodes'][key]['py/tuple'][0]})
+    node_type = parsed_json['nodes'][key]['py/tuple'][0]
+    #only add a cache_size attribute for routers
+    if parsed_json['nodes'][key]['py/tuple'][0] == 'router':
+        #check if router dictionary is empty - cache size is null
+        if bool(parsed_json['nodes'][key]['py/tuple'][1]):
+            #print parsed_json['nodes'][key]['py/tuple'][1]
+            cache_size = parsed_json['nodes'][key]['py/tuple'][1]['cache_size']
+        else:
+            cache_size = 0
+        nodes.append({"name": key, "group":i, "type": node_type, "cache_size" : cache_size})
+    else:
+        nodes.append({"name": key, "group":i, "type": node_type, "cache_size" : "N/A - not a router"})
     i += 1
+
+print len(nodes)
 
 node_dict = {}
 for node in nodes:
-    node_dict[node['name']] = node['group']
+    node_dict[node['name']] = node['group']   
+
 
 edges = []
-
 for j in range(len(parsed_json['edges'])):
     edges.append({"source": node_dict[str(parsed_json['edges'][j][u'py/tuple'][0])], 
                     "target": node_dict[str(parsed_json['edges'][j][u'py/tuple'][1])],
                     "value": 1 })    
+
+print len(edges)
 
 output_format = {}
 output_format['nodes'] = nodes
