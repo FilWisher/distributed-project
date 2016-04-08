ffb40a0c8fb4dff86b7db2499171c5993a1e033f
Hugh O'Brien <obrienhu@tcd.ie>
2016-04-08 21:44:44 +0100

    adding files missed from previous commit

---
+++ icarus/scripts/event_cleanup.py
@@ -0,0 +1,52 @@
+'''
+Cleans up event data from Icarus Simulator
+
+Input format: JSON formats provided by the 'EVENT_TIMELINE' data collector
+
+Output format: JSON object in the form:
+{
+  topology: {
+    nodes: [...],
+    edges: [...]
+  },
+  events: [
+    {"event_type":"...", ...}, ...
+  ]
+}
+'''
+import sys, os, json, jsonpickle
+from icarus.registry import RESULTS_READER
+
+currDir = os.getcwd()
+if currDir not in sys.path:
+    sys.path.append(currDir)
+
+def writefile(filename, output):
+    f = open(filename, 'w')
+    f.write(output)
+    f.close
+
+def reFormat(filename):
+    results = RESULTS_READER['JSON'](filename)
+    timeline = results[0][1]['EVENT_TIMELINE']['TIMELINE']
+    f = open("clean_events.json", 'w')
+
+    if results[0][1]['TOPOLOGY']:
+        nodes = results[0][1]['TOPOLOGY']['TOPOLOGY'].nodes(True)
+        structured_nodes = {}
+        for n in nodes:
+            if 'contents' in n[1]['stack'][1]:
+                n[1]['stack'][1]['contents'] = list(n[1]['stack'][1]['contents'])
+            structured_nodes[n[0]] = n[1]
+
+        topology = {'NODES':structured_nodes,'EDGES':
+                    results[0][1]['TOPOLOGY']['TOPOLOGY'].edges(data = True)}
+        output = {'TOPOLOGY':topology, 'EVENTS':timeline}
+        json.dump(output, f)
+    else:
+        json.dump(timeline, f)
+
+for i in sys.argv[1:]:
+    reFormat(i)
+
+
